c1fc1217b9fe1bbe44118d9ccb099f1e96f8f106
NanYangEvelyn <ny2187@nyu.edu>
2023-09-13 15:24:57 -0400

    15.24

--- sketch.js
+++ sketch.js
@@ -1,5 +1,18 @@
 function setup() {
+    createCanvas(400,600);
+    background(255,123,255);
 }
 
 function draw() {
+    stroke(0);
+    strokeWeight(0);
+    fill("orange");
+    ellipse(100,100,100,100);
+    fill(0,255,0);
+    ellipse(100,300,100,100);
+    stroke(124,213,182);
+    strokeWeight(4);
+    rect(200,200,100,100);
+
+
 }
